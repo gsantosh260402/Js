c96ae2c16021e7e89855130cf2c6b9173dc7bf83
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-23 18:17:41 +0000

    VARIABLES

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "santosh@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur"
+
+// accountId = 2;  // this line is not allowed
+
+console.log(accountEmail);
+
+console.table([
+    accountId , accountEmail , accountPassword , accountCity
+])
+
