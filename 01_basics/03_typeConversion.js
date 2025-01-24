6e0fd9101fc239b7f6ef25157e8238e12de2ff8a
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-24 18:05:42 +0000

    typeConversions2

--- 01_basics/03_typeConversion.js
+++ 01_basics/03_typeConversion.js
@@ -35,3 +35,34 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+
+// *********************** Operations *************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello";
+let str2 = "hitesh";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1"+2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+
+console.log(1 + 2 + "2"); // 32
+
+console.log(3 + 4 * 5 % 3)
+
+
