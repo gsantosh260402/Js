a414efd7debc73d5675a5e9bab2e0d5848f80546
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-24 16:56:52 +0000

    typeConversion

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,37 @@
+/*
+let score = "33abc";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+*/
+
+// ---------- After Conversion to number -----------------
+
+// "33"  => 33
+// "33abc" => NaN
+//  true => 1;
+//  false => 0
+//  null => 0
+
+
+/*
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+*/
+
+// ----------- After conversion to boolean ------------------
+
+// 1 => true ; 0 => false
+// "" => false;
+// "santosh" => true;
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
