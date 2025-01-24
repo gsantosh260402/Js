9eecdc3bb6725b403e37f388b86683264a5ca7b3
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-24 16:01:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ; // treat all js code as newer version
+
+//alert("hello"); // we are using nodejs , not browser
+
+console.log(3 + 3);
+console.log("Hitesh")
+
+// --------------- Datatypes -----------------
+
+// number =>2 to power 53
+// bigInt
+// string
+// boolean
+// null => standalone value
+// undefined => if  variable is not assigned to any value 
+// symbol => unique
+
+// object
+
+console.log(typeof "santosh");
+console.log(typeof null);
+console.log(typeof undefined);
