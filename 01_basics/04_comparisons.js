4966d4e3c68a6f6f06812da9f24c87610b0aff2c
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-24 18:20:23 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// ------------------- comaparison in different datatypes ------------------
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("2" == 2); // true;
+
+console.log("2" === 2) // false;  --> comapare both type and value
