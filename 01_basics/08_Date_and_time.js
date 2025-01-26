278c6007a56ca39bdfe2eb690831f612d897c18c
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-26 16:16:35 +0000

    Date and time

---
+++ 01_basics/08_Date_and_time.js
@@ -0,0 +1,16 @@
+let myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof(myDate));
+
+//let myCreateDate = new Date("2023-01-14")
+
+let myCreateDate = new Date("01-14-2023")
+console.log(myCreateDate.toLocaleString());
+
+console.log(myCreateDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+ 
+
+
