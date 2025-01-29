13d8c81d37cc3d673eb78f36775e807eec9a95eb
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-29 17:37:39 +0000

    minihoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,39 @@ if(true){
 //console.log(a) ; // error
 //console.log(b);  // error
 //console.log(c);  // ans -- 30
+
+function one(){
+    const username = "hitesh";
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    //console.log(website); // website variable can't be accessed  outside it's scope
+    two();
+}
+one();     // inner function can access variables declared in outer function.
+
+
+// +++++++++++++++++ interesting +++++++++++++++++++++++
+
+
+//1 --> function declaration type 1
+console.log(addone(5)); 
+
+function addone(num){
+    return num + 1;
+}
+
+// in this type of declaration function can be called before declaration 
+
+
+
+
+//2 --> function is stored in a variable
+
+
+addTwo(5);  // this is not possible here
+const addTwo = function(num){
+    return num + 2;
+}
+
