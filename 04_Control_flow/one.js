a1fb3db315546a2dae7119f443a198b6c690610a
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-30 18:01:37 +0000

    control statements

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,28 @@
+
+if(2 == "2"){
+    console.log("Executed");
+}
+
+
+// <  , >   , <= ,    >= ,    == ,     != ,  === , !==
+
+
+// falsy value 
+
+//false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+      // Truthy Values
+//************************
+
+//  "0"  , 'false' , " " [string with a space],{} , function(){}
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+val1 = 5 ?? 10;
+
+console.log(val1);
+  
+
+
