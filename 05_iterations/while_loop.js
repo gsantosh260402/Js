15baa9b8aab3a111808498cb58b211a6c7471984
gsantosh260402 <112861746+gsantosh260402@users.noreply.github.com>
2025-01-31 17:39:28 +0000

    while and do-while loops

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,26 @@
+// while loop
+
+let i = 1;
+
+while (i<=5 ) {
+    console.log(`value is : ${i}`);
+    i++;
+}
+
+
+myArray = ["flash" , "batman" , "superman"];
+let index = 0;
+while(index < myArray.length){
+    console.log(`value is : ${myArray[index]}`);
+    index++;
+}
+
+
+// do-while loop
+
+let score = 7;
+
+do{
+   console.log(`value is : ${score}`);
+   score++;
+}while(score<=5);
